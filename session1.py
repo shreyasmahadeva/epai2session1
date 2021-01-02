25cef72cb0e7e910f1b3beaaef26f696974d0ba5
shreyasmahadeva <57949068+shreyasmahadeva@users.noreply.github.com>
2021-01-02 17:20:36 +0530

    Updated the session1.py

--- session1.py
+++ session1.py
@@ -1,12 +1,12 @@
-def myFunc():
+def myfunc():
     string='Cppsecrets'
     n=len(string)
     arr=[]
     for i in range(n):
-      for j in range(i+1,n+1):
-          a=string[i:j]
-          arr.append(a)
-	print(arr)
+        for j in range(i+1,n+1):
+            a=string[i:j]
+            arr.append(a)
+    print(arr)
 
-def my_Func():
+def my_func():
     pass
